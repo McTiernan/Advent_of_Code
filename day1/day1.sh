ec59a8e96f7fd5bea01d496c8e808d0caafb2661
Ethan McTiernan <ethan.mctiernan@gmail.com>
2015-12-15 14:20:10 +0000

    Done Part two and added the puzzle decription

--- day1/day1.sh
+++ day1/day1.sh
@@ -2,6 +2,9 @@
 # data file
 INPUT=$1
 Floor=0
+count=1
+charlocation=0
+hasBeenSet=false
 [ $# -eq 0 ] && { echo "Usage whattt : $0 filename"; exit 1; }
  
 # Make sure file exits else die 
@@ -14,7 +17,12 @@ do
 	else 
 		(( Floor-- ))
 	fi
-
+	if [[ "$Floor" == "-1" && "$hasBeenSet" == "false" ]]; then
+		charlocation=$count
+		hasBeenSet=true
+	fi
+(( count++ ))
 done < "$INPUT"
 
 echo "Santa is on floor $Floor"
+echo "Character Position that makes santa go to -1 is: $charlocation"
