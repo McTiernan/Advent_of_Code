151a485d0b43e3b8c6477c66068fd70162b2693d
Ethan McTiernan <ethan.mctiernan@gmail.com>
2015-12-15 14:49:27 +0000

    Done second part of puzzle and added a description of the puzzle

--- day3/day3.java
+++ day3/day3.java
@@ -6,6 +6,7 @@ import java.awt.Point;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class day3{
 
@@ -13,51 +14,73 @@ public class day3{
 
 
     public static void main(String[] args) throws IOException {
-        readInFile();
-        System.out.println("Santa delivered at least one present to " + whereSantasBeen.size() + " houses" );
+        System.out.println("Would you like to solve puzzle 1 or 2? Answer with the puzzle number.");
+        Scanner input = new Scanner(System.in);
+        int answer = input.nextInt();
+
+        readInFile(answer);
+        System.out.println("Santa delivered at least one present to: " + whereSantasBeen.size() + " houses" );
+        //System.out.println(whereSantasBeen);
     }
 
-    static void readInFile() throws IOException {
+    static void readInFile(int puzzle) throws IOException {
         Point startingLoc = new Point();
         startingLoc.setLocation(0, 0);
+        //System.out.println("santasLoc first = " + santasLoc);
         whereSantasBeen.addLast(startingLoc);
 
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
                         new FileInputStream("/Users/ethanmctiernan/Development/Advent_of_Code/day3/input.txt"), Charset.forName("UTF-8")));
         int c;
-        Point lastLoc = startingLoc;
+        Point santaslastLoc = startingLoc;
+        Point roboslastLoc = startingLoc;
+        int count = 1;
         while ((c = reader.read()) != -1) {
+            Point lastLoc;
+            if(count%2!=0&&puzzle==2){
+                lastLoc=roboslastLoc;
+            }
+            else lastLoc = santaslastLoc;
             Point santasLoc = new Point();
             char character = (char) c;
             if (character == 'v') {
 
-                santasLoc.setLocation(lastLoc.getX(),lastLoc.getY()-1);
+                santasLoc.setLocation(lastLoc.getX(), lastLoc.getY() - 1);
                 lastLoc = santasLoc;
-                if(!hasSantaBeenHere(santasLoc)) {
-                    whereSantasBeen.add(santasLoc);
-                }
-            }if (character == '^') {
-                santasLoc.setLocation(lastLoc.getX(),lastLoc.getY()+1);
-                lastLoc = santasLoc;
-
-                if(!hasSantaBeenHere(santasLoc)) {
-                    whereSantasBeen.add(santasLoc);
-                }
-            }if (character == '>') {
-                santasLoc.setLocation(lastLoc.getX()+1,lastLoc.getY());
-                lastLoc = santasLoc;
-
-                if(!hasSantaBeenHere(santasLoc)) {
-                    whereSantasBeen.add(santasLoc);
-                }
-            }if (character == '<') {
-                santasLoc.setLocation(lastLoc.getX()-1,lastLoc.getY());
-                lastLoc = santasLoc;
-                if(!hasSantaBeenHere(santasLoc)) {
+                //System.out.println(santasLoc.getX() + " " + santasLoc.getY());
+                if (!hasSantaBeenHere(santasLoc)) {
                     whereSantasBeen.add(santasLoc);
                 }
             }
+            if (character == '^') {
+                santasLoc.setLocation(lastLoc.getX(), lastLoc.getY() + 1);
+                lastLoc = santasLoc;
+
+                if (!hasSantaBeenHere(santasLoc)) {
+                    whereSantasBeen.add(santasLoc);
+                }
+            }
+            if (character == '>') {
+                santasLoc.setLocation(lastLoc.getX() + 1, lastLoc.getY());
+                lastLoc = santasLoc;
+
+                if (!hasSantaBeenHere(santasLoc)) {
+                    whereSantasBeen.add(santasLoc);
+                }
+            }
+            if (character == '<') {
+                santasLoc.setLocation(lastLoc.getX() - 1, lastLoc.getY());
+                lastLoc = santasLoc;
+                if (!hasSantaBeenHere(santasLoc)) {
+                    whereSantasBeen.add(santasLoc);
+                }
+            }
+            if(count%2!=0&&puzzle==2){
+                roboslastLoc=lastLoc;
+            }
+            else santaslastLoc = lastLoc;
+            count++;
         }
 
     }
@@ -65,9 +88,11 @@ public class day3{
         boolean toReturn = false;
 
         for (Point p : whereSantasBeen) {
+            //System.out.println(" p  = " + p +" santasLoc: "  + santasLoc);
             if(p.equals(santasLoc)) {toReturn = true; break;}
             else toReturn = false;
         }
+        //System.out.println("toReturn = " + toReturn);
         return toReturn;
     }
 
